5a0d691bb97d9740a66af865dd2bcd6fff8818ac
Caleb-Script <caleb-script@outlook.de>
2025-06-11 15:57:56 +0200

    remove analyze

--- src/test/java/com/omnixys/account/AccountApplicationTests.java
+++ src/test/java/com/omnixys/account/AccountApplicationTests.java
@@ -1,5 +1,7 @@
 package com.omnixys.account;
 
+import com.omnixys.account.utils.Env;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -7,6 +9,11 @@ import org.springframework.context.annotation.Import;
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class AccountApplicationTests {
+
+	@BeforeAll
+	protected static void setup() {
+		new Env();
+	}
 
 	@Test
 	void contextLoads() {
